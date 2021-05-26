d925d68efd8cf62c08f91e47af9be660e20286e9
Sandip124 <chaudharypankaj.official@gmail.com>
2021-05-26 20:10:36 +0545

    display thumbnail in gallery

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,19 +1,30 @@
-window.console.log("hello from Javascript");
-// var pets = new Array("cat","dog","cow");
-// var firstPet = pets[0];
-// window.console.log("The first pet is at index 0. It is a " + firstPet);
-// var index = 0;
-// while(index<pets.length){
-//     window.console.log(pets[index]);
-//     index = index+1;
-// }
 
 function init(){
-    // window.console.log("Welcome, user with new browser");
     var lightboxElements = "<div id='lightbox'>";
     lightboxElements += "<div id='overlay' class='hidden'></div>";
     lightboxElements += "<img class='hidden' id='big-image' />";
     lightboxElements += "</div>";
     document.querySelector("body").innerHTML += lightboxElements;
+    prepareThumbs();
 }
+
+function toggle(){
+    window.console.log("show or hide a big image");
+}
+
+function prepareThumbs(){
+    var liElements = document.querySelectorAll("ul#images li");
+    var i=0;
+    var image, li;
+    while(i<liElements.length){
+        li = liElements[i];
+        li.setAttribute("class","lightbox");
+        image = li.querySelector("img");
+        image.addEventListener("click", toggle, false);
+        i +=1;
+        
+    }
+}
+
+
 document.addEventListener("DOMContentLoaded", init, false);
